d35fcd710876ca6c6e6a1db814dc445ac0e0a713
Nozomi Hosone <otofuto@gmail.com>
2021-06-20 15:09:40 +0900

    fix

--- pkg/human/human.go
+++ pkg/human/human.go
@@ -5,11 +5,19 @@ import (
 	"github.com/otofuto/people/pkg/database"
 )
 
+type Human struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+	Age int `json:"age"`
+	Tension int `json:"tension"`
+}
+
 type StringData struct {
 	Human int `json:"human"`
 	Data string `json:"data"`
-	Tension int `json:"tension"`
+	Feel int `json:"feel"`
 	Equal string `json:"equal"`
+	Group []string `json:"group"`
 	Big []string `json:"big"`
 	Not []string `json:"not"`
 	Then []string `json:"then"`
@@ -32,7 +40,7 @@ func (sd *StringData) Insert() error {
 	db := database.Connect()
 	defer db.Close()
 
-	sql := "insert into `string_data` (`human`, `data`, `tension`) select * from (?, ?, ?) as tmp where not exists (select * from `string_data` where `human` = ? and `data` = ?) limit 1"
+	sql := "insert into `string_data` (`human`, `data`, `feel`) select * from (?, ?, ?) as tmp where not exists (select * from `string_data` where `human` = ? and `data` = ?) limit 1"
 	ins, err := db.Prepare(sql)
 	if err != nil {
 		log.Println("human.go (sd *StringData) Insert()")
@@ -42,7 +50,7 @@ func (sd *StringData) Insert() error {
 	}
 	defer ins.Close()
 
-	_, err = ins.Exec(&sd.Human, &sd.Data, &sd.Tension, &sd.Human, &sd.Data)
+	_, err = ins.Exec(&sd.Human, &sd.Data, &sd.Feel, &sd.Human, &sd.Data)
 	if err != nil {
 		log.Println("human.go (sd *StringData) Insert()")
 		log.Println(err)
@@ -53,23 +61,23 @@ func (sd *StringData) Insert() error {
 	return nil
 }
 
-func (sd *StringData) AddBig(big string) error {
+func (sd *StringData) AddParam(param, val string) error {
 	db := database.Connect()
 	defer db.Close()
 
-	sql := "insert into `string_data_than` (`data`, `big`) select * from (?, ?) as tmp where not exists (select * from `string_data_than` where `data` = ? and `big` = ?) limit 1"
+	sql := "insert into `string_data_than` (`human`, `data`, `" + param + "`) select * from (?, ?, ?) as tmp where not exists (select * from `string_data_than` where `human` = ? and `data` = ? and `" + param + "` = ?) limit 1"
 	ins, err := db.Prepare(sql)
 	if err != nil {
-		log.Println("human.go (sd *StringData) Big(big string)")
+		log.Println("human.go (sd *StringData) AddParam(param, val string)")
 		log.Println(err)
 		log.Println(sql)
 		return err
 	}
 	defer ins.Close()
 
-	_, err = ins.Exec(&sd.Data, &big, &sd.Data, &big)
+	_, err = ins.Exec(&sd.Human, &sd.Data, &val, &sd.Human, &sd.Data, &val)
 	if err != nil {
-		log.Println("human.go (sd *StringData) Big(big string)")
+		log.Println("human.go (sd *StringData) AddParam(param, val string)")
 		log.Println(err)
 		log.Println(sql)
 		return err
